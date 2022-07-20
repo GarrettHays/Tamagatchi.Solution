2f1149b65657ae134a132cd1a2af2349269871fc
Alex Shevlin <alexshevlin1@gmail.com>
2022-07-20 12:06:45 -0700

    update constructor to set random int for stats and add Id.
    Co-authored-by: Garrett Hays <garrettlhays@gmail.com>
    Co-authored-by: Matt Herbert <matthew.f.herbert@gmail.com>

--- Tamagatchi/Models/BaseTamagatchi.cs
+++ Tamagatchi/Models/BaseTamagatchi.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace Tamagatchi.Models
 {
@@ -10,7 +11,45 @@ namespace Tamagatchi.Models
     public int Training { get; set; }
     public int Discipline { get; set; }
     public int Age { get; set; }
+    public int Height { get; set; }
     public int Weight { get; set; }
+    public int Id { get; set; }
+    private static List<BaseTamagatchi> _types = new List<BaseTamagatchi> {};
+
+    public BaseTamagatchi(string name)
+    {
+      Name = name;
+      //random 1=10 for Hunger, Happy, Training, and Discipline
+      Hunger = this.GetStat(1, 10);
+      Happy = this.GetStat(1, 10);
+      Training = this.GetStat(1, 10);
+      Discipline = this.GetStat(1, 10);
+      Age = 0; //connect to local machine time
+      //random 25-40 for height
+      Height = this.GetStat(25, 40);
+      //random 20-30 for weight
+      Weight = this.GetStat(20, 30);
+
+      _types.Add(this);
+      Id = _types.Count;
+    }
+
+    public static List<BaseTamagatchi> GetAll()
+    {
+        return _types;
+    }
+
+    public int GetStat(int min, int max)
+    {
+      Random rnd = new Random();
+      int stat = rnd.Next(min, max);
+      return stat;
+    }
+
+    public static BaseTamagatchi Find(int searchId)
+    {
+      return _types[searchId - 1];
+    }
 
 
   }
