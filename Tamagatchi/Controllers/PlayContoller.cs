48df85a120ce54695eb72bb08350627ae2949dbe
Alex Shevlin <alexshevlin1@gmail.com>
2022-07-20 14:31:11 -0700

    update controller to implement feed.
    Co-authored-by: Garrett Hays <garrettlhays@gmail.com>
    Co-authored-by: Matt Herbert <matthew.f.herbert@gmail.com>

--- Tamagatchi/Controllers/PlayContoller.cs
+++ Tamagatchi/Controllers/PlayContoller.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Tamagatchi.Models;
+using System;
 using System.Collections.Generic;
 
 namespace Tamagatchi.Controllers
@@ -27,10 +28,35 @@ namespace Tamagatchi.Controllers
     }
 
     [HttpGet("/play/{id}")]
-    public ActionResult Show(int id)
+    public ActionResult Show(int id, string button)
     {
       BaseTamagatchi foundMinion = BaseTamagatchi.Find(id);
+      if (button == "feed")    
+      {
+        if (foundMinion.Hunger < 10 && foundMinion.Hunger > 0)
+        {
+          foundMinion.Hunger = foundMinion.Hunger + 1;
+          foundMinion.CheckFood = "You fed " + foundMinion.Name;
+        }
+        else if (foundMinion.Hunger == 10)
+        {
+          foundMinion.CheckFood = foundMinion.Name + " is full!";
+        }
+        else
+        {
+          foundMinion.CheckFood = foundMinion.Name + " is " + foundMinion.Status + " you can't feed a corpse.";
+        }
+      }
       return View(foundMinion);
     }
+                      //6
+    // [HttpPost("/play/{id}")]        //6
+    // public ActionResult FeedTest()
+    // { 
+
+    //   //else if (button == "fappy")
+    //   return RedirectToAction("Show");
+    // }
+
   }
 }
